8599217424dba93f0c22dee9780e142a9087491f
Diego Villamil <dmvillamil@up.edu.ph>
2023-11-19 13:45:03 +0800

    instructions move through the pipeline w o rules

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #CMSC 132 AB2L
 #Exercise 9
 
+import copy
+
+
 #Read instructions from given filename
 def readIns(filename):
   instructions = []
@@ -16,8 +19,28 @@ def readIns(filename):
     instructions.append(temp)
     print(temp)
     
-
+  return instructions
 
 #Start of main
-readIns("input.in")
+ins = readIns("input.in")
 pipeline = [["F",None],["D",None],["E",None],["M",None],["W",None]]
+finished = []
+numIns = len(ins)
+
+while(len(finished) != numIns):
+  if(len(ins) != 0):
+    pipeline[0][1] = ins.pop(0)
+  temp = None
+  for i in range(4,0,-1):
+    if((i == 4) & (pipeline[i][1] != None)):
+      finished.append(copy.deepcopy(pipeline[i][1]))
+      pipeline[i][1] = None
+    
+    pipeline[i][1] = copy.deepcopy(pipeline[i-1][1])
+    pipeline[i-1][1] = None
+  for stage in pipeline:
+    print(stage)
+  print("******************************************")
+
+for inst in finished:
+  print(inst)
